bbad57228bb89b43021092f3c8d8707d773c18fd
NatalieR2026 <natalie.radin2026@gbwl.org>
2026-04-22 13:42:18 +0000

    4/22

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create instance of Flask
 app = Flask(__name__)
 
-# Homepage route
-@app.route("/")
-def index():
-    return render_template("index.html")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+# Define model of a to-do list TASK object
+class Task(db.Model):
+    # db.Column represents a col in the database
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+
+    # Homepage route
+    @app.route("/")
+    def index():
+        # Add new Task to database
+        if request.method == 'POST':
+            task_content = request.form['content']
+            new_task = Task(content=task_content)
+            # Put new Task in database
+            try:
+                db.session.add(new_task)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'Error adding task'
+        # Select all Tasks from database
+        all_tasks = Task.query.all()
+        return render_template('index.html', tasks=all_tasks)
 
 # (if you closed your terminal, open it again with CTRL + `)
 # TO STOP click CTRL + C in the TERMINAL
 # RUN THE APP (or type flask run in terminal)
-if __name__ == '__main__':
+# Create the database in the main method
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
